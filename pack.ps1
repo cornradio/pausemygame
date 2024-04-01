21224d3781d2cc23178de33af1917c0ea1c4b0c3
Name <kasusaland@gmail.com>
2024-04-01 22:58:04 +0800

    upd

---
+++ pack.ps1
@@ -0,0 +1,8 @@
+Remove-Item -Path .\dist\* -Recurse -Force
+xcopy  afont.ttf dist\ /y
+xcopy  game_name.txt dist\ /y
+xcopy  pssuspend.exe dist\ /y
+
+pyinstaller  --exclude=pyinstaller --exclude=pandas --noconsole  --exclude=numpy --exclude=libcrypto --exclude=PIL --onefile --icon=icon.ico  main.py
+# //compress \dist\lightspeed to lightspeed.zip using pwsh
+Compress-Archive -Path .\dist\ -DestinationPath .\pausemygame_vx.x.zip -Force
